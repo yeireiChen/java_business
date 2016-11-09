6395a4709e1f3d4a8c10a670d31ea3b780a935e5
yeireiChen <randy6409@gmail.com>
2016-11-09 17:22:03 +0800

    Revert "30_midern"
    This reverts commit 38fa1cb7bc1ea27a59fc291d97f5885d1c1cf0d9.

--- 30_midtern/src/course/java/spring/data/springdata/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package course.java.spring.data.springdata;
-
-import org.springframework.data.repository.CrudRepository;
-
-import course.java.spring.data.entity.Account;
-
-public interface AccountRepository extends CrudRepository<Account, Integer> {
-
-}
--- 30_midtern/src/course/java/spring/data/springdata/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package course.java.spring.data.springdata;
-
-import org.springframework.data.repository.CrudRepository;
-
-import course.java.spring.data.entity.Transaction;
-
-public interface TransactionRepository extends CrudRepository<Transaction, Integer> {
-
-}
